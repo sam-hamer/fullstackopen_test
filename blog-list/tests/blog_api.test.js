6c7360cb8958b9d7e5c557a61890b31bce80ad87
Sam Hamer <samuel.hamer@outlook.com>
2024-07-13 12:28:21 -0500

    temp

---
+++ blog-list/tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const { test, after, beforeEach, describe } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+
+const helper = require("./test_helper");
+const app = require("../app");
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+describe("api tests", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+    const promiseArray = blogObjects.map((blog) => blog.save());
+    await Promise.all(promiseArray);
+  });
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  after(async () => {
+    await mongoose.connection.close();
+  });
+});
