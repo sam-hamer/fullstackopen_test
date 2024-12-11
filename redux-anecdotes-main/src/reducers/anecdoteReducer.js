4ddfd065c7520c07fabd9b998e05a0eb9a3a4a13
Sam Hamer <samuel.hamer@outlook.com>
2024-12-11 09:09:11 -0500

    ex 6.11

--- redux-anecdotes-main/src/reducers/anecdoteReducer.js
+++ redux-anecdotes-main/src/reducers/anecdoteReducer.js
@@ -22,7 +22,7 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject);
 
 const anecdoteSlice = createSlice({
-  name: "notes",
+  name: "anecdotes",
   initialState,
   reducers: {
     createAnecdote(state, action) {
