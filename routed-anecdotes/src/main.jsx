d87b656a465b6e89bd5f1e00bbd98f465bb727a3
Sam Hamer <samuel.hamer@outlook.com>
2024-12-30 12:16:45 -0600

    ex 7.1 - 7.3

--- routed-anecdotes/src/main.jsx
+++ routed-anecdotes/src/main.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
